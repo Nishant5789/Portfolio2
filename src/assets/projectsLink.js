6450110ec4839c37011afd107a0a89dcc93e0dcd
Nishant Bhandari <94158059+Nishant5789@users.noreply.github.com>
2024-07-11 22:02:37 +0530

    Update projectsLink.js

--- src/assets/projectsLink.js
+++ src/assets/projectsLink.js
@@ -1,36 +1,36 @@
 const projectslink = [
-  {
-    name: "moviesflix",
-    picture: `https://cdn.jsdelivr.net/gh/Nishant5789/My_Tech_Notes/asset/moviesflix.jpg`,
-    demo: "",
-    code: "https://github.com/Nishant5789/Moviesflix",
-  },
-  {
-    name: "Flight booking",
-    picture: `https://cdn.jsdelivr.net/gh/Nishant5789/My_Tech_Notes/asset/flight_booking.png`,
-    demo: "https://www.linkedin.com/posts/nishant-bhandari-193a83201_flightbooking-webapp-html-activity-7050887761230241792-SWbQ?utm_source=share&utm_medium=member_desktop",
-    code: "https://github.com/Nishant5789/Flight_Booking_project",
-  },
-  {
-    name: "ipllive",
-    picture: `https://cdn.jsdelivr.net/gh/Nishant5789/My_Tech_Notes/asset/iplive.jpg`,
-    demo: "",
-    code: "https://github.com/Nishant5789/liveipl",
-  },
-  {
-    name: "popcornflix",
-    picture: `https://cdn.jsdelivr.net/gh/Nishant5789/My_Tech_Notes/asset/popcornflix.png`,
-    demo: "https://www.youtube.com/watch?v=l0jOgiawMg0",
-    code: "https://github.com/Nishant5789/Popcornflix",
-  },
-  {
-    name: "Ecommerce",
-    picture: `https://cdn.jsdelivr.net/gh/Nishant5789/My_Tech_Notes/asset/moviesflix.jpg`,
-    demo: "https://www.linkedin.com/posts/nishant-bhandari-193a83201_webdevelopment-html-css-activity-6943882671177486336-wOzs?utm_source=share&utm_medium=member_desktop",
-    code: "https://github.com/Nishant5789/Ecommerce_website_CE008_CE021",
-  },
-];
-
-
-
-export default projectslink;
+    {
+      name: "CodeCollaB",
+      picture: `https://cdn.jsdelivr.net/gh/Nishant5789/My_Tech_Notes/asset/codecollab.jpg`,
+      demo: "",
+      code: "https://github.com/Nishant5789/CodeCollaB",
+    },
+    {
+      name: "Flight booking",
+      picture: `https://cdn.jsdelivr.net/gh/Nishant5789/My_Tech_Notes/asset/flight_booking.png`,
+      demo: "https://www.linkedin.com/posts/nishant-bhandari-193a83201_flightbooking-webapp-html-activity-7050887761230241792-SWbQ?utm_source=share&utm_medium=member_desktop",
+      code: "https://github.com/Nishant5789/Flight_Booking_project",
+    },
+    {
+      name: "EcommerceApp",
+      picture: `https://cdn.jsdelivr.net/gh/Nishant5789/My_Tech_Notes/asset/Ecommerce.jpg`,
+      demo: "",
+      code: "https://github.com/Nishant5789/Ecommerce-Frontend",
+    },
+    {
+        name: "chatapp",
+        picture: `https://cdn.jsdelivr.net/gh/Nishant5789/My_Tech_Notes/asset/chatapp.jpg`,
+        demo: "",
+        code: "https://github.com/Nishant5789/chatapp",
+    },
+    {
+      name: "popcornflix",
+      picture: `https://cdn.jsdelivr.net/gh/Nishant5789/My_Tech_Notes/asset/popcornflix.png`,
+      demo: "https://www.youtube.com/watch?v=l0jOgiawMg0",
+      code: "https://github.com/Nishant5789/Popcornflix",
+    },
+  ];
+  
+  
+  
+  export default projectslink;
